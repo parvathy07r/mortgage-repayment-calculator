75951d2db4bb284da1df7aa6f780c59c18f4ac4d
Parvathy R <parvathy07r@gmail.com>
2024-07-24 15:43:51 +0530

    feat: Add form validation for  input fields

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const formData = document.querySelector("#form_data");
+const mortgageAmountError = document.querySelector(".amount-error");
+const mortgageTermError = document.querySelector(".term-error");
+const interestRateError = document.querySelector(".rate-error");
+const mortgageTypeError = document.querySelector(".type-error");
 
 formData.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const form = event.target;
+    const mortgageAmount = form["amount"].value;
+    const mortgageTerm = form["term"].value;
+    const interestRate = form["rate"].value;
+    const repaymentRadioButton = form.querySelector("#repayment").checked;
+    const interestOnlyRadioButton = form.querySelector("#interest").checked;
+
+    if(!mortgageAmount) {
+        mortgageAmountError.innerHTML = "This field is required";
+    }
+
+    if(!mortgageTerm) {
+        mortgageTermError.innerHTML = "This field is required";
+    }
+
+    if(!interestRate) {
+        interestRateError.innerHTML = "This field is required";
+    }
+
+    if(!repaymentRadioButton || ! interestOnlyRadioButton) {
+        mortgageTypeError.innerHTML = "This field is required";
+    } 
+
 });
